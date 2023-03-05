0a0ca3a1770ee20f77c809ffa6bbf26658ed1ce1
lavaxde <127015078+lavaxde@users.noreply.github.com>
2023-03-06 06:42:12 +1030

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex2-0.onrender.com/', {
+    const response = await fetch('https://lava-gpt.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
